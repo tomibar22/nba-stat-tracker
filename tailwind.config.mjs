df362d53d756472ab2cec470a83b6a4b4fd33b5f
tomibar22 <tomibar9@gmail.com>
2025-02-13 17:06:34 +0200

    Fix dependencies and add dark theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
     './src/**/*.{js,jsx}',
   ],
   theme: {
